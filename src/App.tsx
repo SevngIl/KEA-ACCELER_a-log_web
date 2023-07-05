b51ed8dcd67a2046deb8adbe17f4944412eb7260
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-07-05 16:45:42 +0900

    Create landing page framework, create project board framework, create issue modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import AppRouter from "./AppRouter";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return <AppRouter />;
+  return <AppRouter />;
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Dropdown, DropdownButton } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+import FadeIn from "../../animation/FadeIn";
+
+export default function Home() {
+  const navigation = useNavigate();
+
+  return (
+    <>
+      <div className="Home">
+        <div className="page1">
+          <FadeIn className="homeLeftWrapper" delay={100} transitionDuration={800}>
+            <h1 className="catchphrase">
+              A-Log is a better way
+              <br /> to manage team and <br /> build products
+            </h1>
+            <div className="homeBtnWrapper">
+              <Button className="GetStarted hvr-grow" onClick={() => navigation("/projectBoard")}>
+                Get Started
+              </Button>
+            </div>
+          </FadeIn>
+        </div>
+      </div>
+    </>
+  );
+}
