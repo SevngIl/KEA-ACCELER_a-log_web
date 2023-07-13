efd754089f9bce960782b4a2fd3a6a24aa76a927
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-07-13 15:53:02 +0900

    Implement Drag&Drop in Board and Add Drag&Drop effects on Board and issues

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,57 +1,106 @@
 import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./Board.css";
 import IssueModal from "../../components/Modal/IssueModal";
 import { LeftNavSection } from "../../navigation/LeftNavSection";
 
 const BoardColumn = ({ column, issues, handleAddIssue }) => {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const handleShowModal = () => setShowModal(true);
-    const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
-    return (
-        <div className="BoardColumn">
-            {issues[column].map((issue, index) => (
-                <div className="BoardIssue" key={index}>
-                    <div className="BoardIssueContent">{issue.content} </div>
-                    <div className="BoardIssueIMG">{issue.imageDataUrl && <img src={issue.imageDataUrl} alt="issue" style={{ width: "100px", height: "100px" }} />}</div>
+  return (
+    <Droppable droppableId={column}>
+      {(provided, snapshot) => (
+        <div className={`BoardColumn ${snapshot.isDraggingOver ? "shadow-lg" : ""}`} ref={provided.innerRef} {...provided.droppableProps}>
+          {issues[column].map((issue, index) => (
+            <Draggable key={issue.id} draggableId={`draggable-${issue.id}`} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  className={`BoardIssue ${snapshot.isDragging ? "bg-opacity-90 shadow-2xl shadow-gray-400" : ""}`}
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={{ ...provided.draggableProps.style }}
+                >
+                  <div className="BoardIssueContent">{issue.content}</div>
+                  <div className="BoardIssueIMG">
+                    {issue.imageDataUrl && <img src={issue.imageDataUrl} alt="issue" style={{ width: "100px", height: "100px" }} />}
+                  </div>
                 </div>
-            ))}
-            <button className="create-issue-btn" onClick={handleShowModal}>
-                Create Issue
-            </button>
-            <IssueModal show={showModal} handleClose={handleCloseModal} handleAddIssue={handleAddIssue} column={column} />
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+          <button className="create-issue-btn" onClick={handleShowModal}>
+            Create Issue
+          </button>
+          <IssueModal show={showModal} handleClose={handleCloseModal} handleAddIssue={handleAddIssue} column={column} />
         </div>
-    );
+      )}
+    </Droppable>
+  );
 };
 
 const Board = () => {
-    const columns = ["TO DO", "IN PROGRESS", "DONE", "EMERGENCY"];
-    const [issues, setIssues] = useState({ "TO DO": [], "IN PROGRESS": [], DONE: [], EMERGENCY: [] });
+  const columns = ["TO DO", "IN PROGRESS", "DONE", "EMERGENCY"];
+  const [issues, setIssues] = useState({ "TO DO": [], "IN PROGRESS": [], DONE: [], EMERGENCY: [] });
 
-    const handleAddIssue = (issueContent, issueStatus, imageDataUrl) => {
-        // 새 이슈 객체를 생성
-        const newIssue = { content: issueContent, status: issueStatus, imageDataUrl };
+  const handleAddIssue = (issueContent, issueStatus, imageDataUrl) => {
+    // 새 이슈 객체를 생성
+    const newIssue = { id: `${issueStatus}-${new Date().getTime()}`, content: issueContent, status: issueStatus, imageDataUrl };
 
-        // 새 이슈를 적절한 리스트에 추가
-        setIssues((prevIssues) => ({
-            ...prevIssues,
-            [issueStatus]: [...prevIssues[issueStatus], newIssue],
-        }));
-    };
-    return (
-        <div className="ProjectBoard">
-            <div className="ProjectBoardName">Project Board</div>
-            <div className="project-board">
-                {columns.map((column) => (
-                    <div className="BoardColumn-wrapper" key={column}>
-                        <h2 className="BoardColumn-title">{column}</h2>
-                        <BoardColumn column={column} issues={issues} handleAddIssue={handleAddIssue} />
-                    </div>
-                ))}
+    // 새 이슈를 적절한 리스트에 추가
+    setIssues((prevIssues) => ({
+      ...prevIssues,
+      [issueStatus]: [...prevIssues[issueStatus], newIssue],
+    }));
+  };
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const newIssue = { ...issues[source.droppableId][source.index], status: destination.droppableId };
+
+      setIssues((prevIssues) => {
+        const newIssues = { ...prevIssues };
+
+        newIssues[source.droppableId].splice(source.index, 1);
+        newIssues[destination.droppableId].splice(destination.index, 0, newIssue);
+
+        return newIssues;
+      });
+    } else {
+      const newIssues = Array.from(issues[source.droppableId]);
+      const [removed] = newIssues.splice(source.index, 1);
+      newIssues.splice(destination.index, 0, removed);
+
+      setIssues((prevIssues) => ({
+        ...prevIssues,
+        [source.droppableId]: newIssues,
+      }));
+    }
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className="ProjectBoard">
+        <div className="ProjectBoardName">Project Board</div>
+        <div className="project-board">
+          {columns.map((column) => (
+            <div className="BoardColumn-wrapper" key={column}>
+              <h2 className="BoardColumn-title">{column}</h2>
+              <BoardColumn column={column} issues={issues} handleAddIssue={handleAddIssue} />
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </DragDropContext>
+  );
 };
 
 export default Board;
--- src/pages/Timeline/Timeline.js
+++ src/pages/Timeline/Timeline.js
@@ -6,68 +6,66 @@ import Form from "react-bootstrap/Form";
 import "./Timeline.css";
 
 export const Timeline = () => {
-    const [editing, setEditing] = useState(false);
-    const [topic, setTopic] = useState("");
-    const [topics, setTopics] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [topic, setTopic] = useState("");
+  const [topics, setTopics] = useState([]);
 
-    const handleButtonClick = () => {
-        setEditing(true); //text 입력 활성화
-    };
+  const handleButtonClick = () => {
+    setEditing(true); //text 입력 활성화
+  };
 
-    const handleInputChange = (event) => {
-        setTopic(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setTopic(event.target.value);
+  };
 
-    const handleKeyPress = (event) => {
-        //엔터키로 입력
-        if (event.key === "Enter") {
-            setEditing(false);
-            setTopics([...topics, topic]);
-            setTopic("");
-        }
-    };
+  const handleKeyPress = (event) => {
+    //엔터키로 입력
+    if (event.key === "Enter") {
+      setEditing(false);
+      setTopics([...topics, topic]);
+      setTopic("");
+    }
+  };
 
-    const renderTableHeader = () => {
-        const header = ["Topic"];
-        for (let i = 7; i <= 12; i++) {
-            header.push(`${i}월`);
-        }
+  const renderTableHeader = () => {
+    const header = ["Topic"];
+    for (let i = 7; i <= 12; i++) {
+      header.push(`${i}월`);
+    }
 
-        return header.map((month, index) => {
-            return <th key={index}>{month}</th>;
-        });
-    };
+    return header.map((month, index) => {
+      return <th key={index}>{month}</th>;
+    });
+  };
 
-    return (
-        <div style={{ display: "flex" }}>
-            <div className="Timeline">
-                <div className="ProjectTimelineName">Project Timeline</div>
-                <Table responsive>
-                    <thead>
-                        <tr>{renderTableHeader()}</tr>
-                    </thead>
-                    <tbody>
-                        {topics.map((topic, index) => (
-                            <tr key={index}>
-                                <td>{topic}</td>
-                                {Array.from({ length: 6 }).fill(<td></td>)}
-                            </tr>
-                        ))}
-                        <tr>
-                            <td>
-                                {editing ? (
-                                    <Form.Control type="text" value={topic} onChange={handleInputChange} onKeyPress={handleKeyPress} />
-                                ) : (
-                                    <Button className="create-topic-btn" onClick={handleButtonClick}>
-                                        Create Topic
-                                    </Button>
-                                )}
-                            </td>
-                            {Array.from({ length: 6 }).fill(<td></td>)}
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Timeline">
+      <div className="ProjectTimelineName">Project Timeline</div>
+      <Table responsive>
+        <thead>
+          <tr>{renderTableHeader()}</tr>
+        </thead>
+        <tbody>
+          {topics.map((topic, index) => (
+            <tr key={index}>
+              <td>{topic}</td>
+              {Array.from({ length: 6 }).fill(<td></td>)}
+            </tr>
+          ))}
+          <tr>
+            <td>
+              {editing ? (
+                <Form.Control type="text" value={topic} onChange={handleInputChange} onKeyPress={handleKeyPress} />
+              ) : (
+                <Button className="create-topic-btn" onClick={handleButtonClick}>
+                  Create Topic
+                </Button>
+              )}
+            </td>
+            {Array.from({ length: 6 }).fill(<td></td>)}
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 };
