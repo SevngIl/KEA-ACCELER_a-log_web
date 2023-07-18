eabac84e1d5f83aeca700c0c80eae8791c87d1cc
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-07-18 15:31:56 +0900

    Change LeftNavSection css and Landingpage css

--- src/navigation/LeftNavSection.tsx
+++ src/navigation/LeftNavSection.tsx
@@ -1,27 +1,38 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import "./LeftNavSection.css";
 import { Button } from "react-bootstrap";
+import logo from "../assets/logo/alog-logo.png";
 
 export const LeftNavSection = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="LeftNavSection">
-            <div className="container">
-                <div>image</div>
-                <div>A-Log</div>
-                <div>Planning</div>
-                <div>
-                    <Button onClick={() => navigate("/Timeline")}>TimeLine</Button>
-                </div>
-                <div>
-                    <Button onClick={() => navigate("/Board")}>Board</Button>
-                </div>
-                <div>Release</div>
-                <div>
-                    <Button onClick={() => navigate("/ReleaseNote")}>Notes</Button>
-                </div>
-            </div>
-            <Outlet />
+  const navigate = useNavigate();
+  return (
+    <div className="LeftNavSection">
+      <div className="container">
+        <div className="head_container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="A-Log">A-Log</div>
         </div>
-    );
+
+        <div className="planning_container">
+          <div className="planning">Planning</div>
+          <div className="Timeline">
+            <Button onClick={() => navigate("/Timeline")}>TimeLine</Button>
+          </div>
+          <div className="Board">
+            <Button onClick={() => navigate("/Board")}>Board</Button>
+          </div>
+        </div>
+
+        <div className="release_container">
+          <div className="release">Release</div>
+          <div className="Notes">
+            <Button onClick={() => navigate("/ReleaseNote")}>Notes</Button>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
 };
