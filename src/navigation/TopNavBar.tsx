1c5d7e8a58d2744c696e8aed3217b86fd7506b58
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-07-27 12:45:38 +0900

    Change Alarm css

--- src/navigation/TopNavBar.tsx
+++ src/navigation/TopNavBar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,97 +8,109 @@ import { Button } from "react-bootstrap";
 import Logo from "../assets/logo/alog-logo.png";
 import { useNavigate } from "react-router-dom";
 import { TextButton } from "../components/Buttons";
+
+import Notification from "../components/Notification/Notification";
 import { AuthenticationContext } from "../service/authentication/authentication.context";
 
-const LoggedInNav = () => {
-    const { OnLogout } = useContext(AuthenticationContext);
-    return (
-        <>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <NavDropdown title="내 작업" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="프로젝트" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="팀" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                    <Button variant="outline-primary">만들기</Button>
-                </Nav>
-            </Navbar.Collapse>
-            <Navbar.Collapse className="justify-content-end">
-                <TextButton>알람</TextButton>
-                <TextButton>설정</TextButton>
-                <TextButton>프로필</TextButton>
-                <TextButton
-                    onClick={() => {
-                        OnLogout();
-                    }}
-                >
-                    로그아웃
-                </TextButton>
-            </Navbar.Collapse>
-        </>
-    );
+interface LoggedInNavProps {
+  onNotificationClick: () => void;
+}
+
+const LoggedInNav = ({ onNotificationClick }: LoggedInNavProps) => {
+  const { OnLogout } = useContext(AuthenticationContext);
+  return (
+    <>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavDropdown title="내 작업" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="프로젝트" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="팀" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <Button variant="outline-primary">만들기</Button>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse className="justify-content-end">
+        <TextButton onClick={onNotificationClick}>알람</TextButton>
+        <TextButton>설정</TextButton>
+        <TextButton>프로필</TextButton>
+        <TextButton
+          onClick={() => {
+            OnLogout();
+          }}
+        >
+          로그아웃
+        </TextButton>
+      </Navbar.Collapse>
+    </>
+  );
 };
 
 const LoggedOutNav = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <NavDropdown title="Company" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-            <Navbar.Collapse className="justify-content-end">
-                <TextButton onClick={() => navigate("/login")}>Sign In</TextButton>
-                <TextButton onClick={() => navigate("/registerform")}>Join</TextButton>
-            </Navbar.Collapse>
-        </>
-    );
+  const navigate = useNavigate();
+  return (
+    <>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavDropdown title="Company" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse className="justify-content-end">
+        <TextButton onClick={() => navigate("/login")}>Sign In</TextButton>
+        <TextButton onClick={() => navigate("/registerform")}>Join</TextButton>
+      </Navbar.Collapse>
+    </>
+  );
 };
+
 export const TopNavBar = () => {
-    const { isLogin } = useContext(AuthenticationContext);
-    const location = useLocation();
-    const navigate = useNavigate();
-    return (
-        <div className="TopNavBar">
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand
-                        onClick={() => {
-                            navigate("/");
-                        }}
-                    >
-                        <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-                        A-Log
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    {isLogin === true ? <LoggedInNav /> : <LoggedOutNav />}
-                </Container>
-            </Navbar>
-            <Outlet />
-        </div>
-    );
+  const { isLogin } = useContext(AuthenticationContext);
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleNotificationClick = () => {
+    setShowNotification(true);
+  };
+
+  const handleCloseNotification = () => {
+    setShowNotification(false);
+  };
+
+  return (
+    <div className="TopNavBar">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>
+            <img src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+            A-Log
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          {isLogin === true ? <LoggedInNav onNotificationClick={handleNotificationClick} /> : <LoggedOutNav />}
+        </Container>
+      </Navbar>
+      <Notification show={showNotification} handleClose={handleCloseNotification} />
+      <Outlet />
+    </div>
+  );
 };
