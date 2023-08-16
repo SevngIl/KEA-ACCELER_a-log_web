f19cb43a35258e5d016329dcbdf5117e47b46506
masibasi <jimin_lee98@naver.com>
2023-08-16 23:30:04 +0700

    css : profile fade in

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,22 +3,22 @@ import "./Profile.css";
 import ProfileImg from "../../assets/images/profile.png";
 import { useContext } from "react";
 import { AuthenticationContext } from "../../service/authentication/authentication.context";
+import FadeIn from "../../animation/FadeIn";
 export const Profile = () => {
-    const { userData } = useContext(AuthenticationContext);
-    console.log(userData);
-    return (
-        <div className="Profile">
-            <FloatingWrapper>
-                <div className="leftContainer">
-                    <img className="profileImg" src={ProfileImg} />
-                    <div>{userData.userEmail}</div>
-                    <div>{userData.userNN}</div>
-                </div>
-                <div className="rightContainer">
-                    <div>패스워드</div>
-                    <div>회원 탈퇴</div>
-                </div>
-            </FloatingWrapper>
+  const { userData } = useContext(AuthenticationContext);
+  console.log(userData);
+  return (
+    <FadeIn className="Profile" childClassName={"childClassName"}>
+      <FloatingWrapper className="floatingWrapper">
+        <div className="leftContainer">
+          <img className="profileImg" src={ProfileImg} />
+          <h5>이메일</h5>
+          <p>{userData.userEmail}</p>
+          <h5>닉네임</h5>
+          <p>{userData.userNN}</p>
         </div>
-    );
+        <div className="rightContainer"></div>
+      </FloatingWrapper>
+    </FadeIn>
+  );
 };
