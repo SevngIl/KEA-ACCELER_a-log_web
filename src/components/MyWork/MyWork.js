2f6d01724f65c3b3fc587618969d0b1339ef6030
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-08-25 16:29:05 +0900

    test

--- src/components/MyWork/MyWork.js
+++ src/components/MyWork/MyWork.js
@@ -19,32 +19,30 @@ function MyWork({ show, handleClose }) {
 
   const [tasks, setTasks] = useState([]);
 
-  const issueAssigneePk = userData ? userData.userPk : null;
+  const issueAssigneePk = userData ? userData.userPk : 0;
   const page = 0;
   const size = 10;
 
   useEffect(() => {
-    if (issueAssigneePk) {
-      GetMyIssues(issueAssigneePk, page, size, userToken)
-        .then((res) => {
-          const todoIssues = res.data.filter((issue) => issue.issueStatus === "TODO");
-          const inProgressIssues = res.data.filter((issue) => issue.issueStatus === "INPROGRESS");
+    GetMyIssues(issueAssigneePk, page, size, userToken)
+      .then((res) => {
+        const todoIssues = res.data.filter((issue) => issue.issueStatus === "TODO");
+        const inProgressIssues = res.data.filter((issue) => issue.issueStatus === "INPROGRESS");
 
-          setTasks([
-            {
-              type: "진행중",
-              issues: inProgressIssues.map((issue) => ({ content: issue.issueDescription })),
-            },
-            {
-              type: "해야 할 일",
-              issues: todoIssues.map((issue) => ({ content: issue.issueDescription })),
-            },
-          ]);
-        })
-        .catch((err) => {
-          // 오류 처리 로직
-        });
-    }
+        setTasks([
+          {
+            type: "진행중",
+            issues: inProgressIssues.map((issue) => ({ content: issue.issueDescription })),
+          },
+          {
+            type: "해야 할 일",
+            issues: todoIssues.map((issue) => ({ content: issue.issueDescription })),
+          },
+        ]);
+      })
+      .catch((err) => {
+        // 오류 처리 로직
+      });
   }, []);
 
   return (
